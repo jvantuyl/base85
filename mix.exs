67383b6a676eeec875f64e0c972e3423b85d9cb5
Jayson Vantuyl <jvantuyl@aggressive.ly>
2026-01-19 02:35:34 -0800

    add tools for runtime tracing, delete comments

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Base85.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :runtime_tools]
     ]
   end
 
@@ -29,8 +29,6 @@ defmodule Base85.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.23", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 
